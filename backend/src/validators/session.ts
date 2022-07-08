005d46d3124d2dec04cf9964c0b38f87fe69c6f4
Elton Moreno <elton989@hotmail.com>
2022-07-07 23:54:21 -0100

    validator text fix

--- backend/src/validators/session.ts
+++ backend/src/validators/session.ts
@@ -8,8 +8,8 @@ const loginValidator = [
       "Username must be between 4 and 20 characters and contain only letters and numbers"
     ),
   body("password")
-    .isLength({ min: 4, max: 20 })
-    .withMessage("Password must be between 4 and 20 characters"),
+    .isLength({ min: 4 })
+    .withMessage("Password must be at least 4 characters"),
 ];
 
 export { loginValidator };
--- backend/src/validators/users.ts
+++ backend/src/validators/users.ts
@@ -4,17 +4,19 @@ import User from "../models/user";
 const createUserValidator = [
   body("username")
     .isLength({ min: 4, max: 20 })
-    .withMessage("username must be at least 8 characters"),
+    .withMessage(
+      "Username must be between 4 and 20 characters and contain only letters and numbers"
+    ),
   body("password")
-    .isLength({ min: 4, max: 20 })
-    .withMessage("password must be at least 8 characters"),
+    .isLength({ min: 4 })
+    .withMessage("Password must be at least 4 characters"),
   body("confirmPassword")
-    .isLength({ min: 4, max: 20 })
-    .withMessage("confirmPassword must be at least 8 characters"),
+    .isLength({ min: 4 })
+    .withMessage("Confirm Password must be at least 4 characters"),
   body("username").custom((value: string) => {
     return User.findOne({ username: value }).then((user) => {
       if (user) {
-        return Promise.reject("username already used");
+        return Promise.reject("Username already used");
       }
     });
   }),
